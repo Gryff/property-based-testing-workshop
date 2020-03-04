76f5ebe07591a762efad6d610542e9351be77512
Gryff <gryffskin@gmail.com>
2020-03-04 14:16:48 +0000

    Add property based test for array sort

--- nodejs/test/sort.test.js
+++ nodejs/test/sort.test.js
@@ -1,7 +1,23 @@
+const fc = require('fast-check')
+
 describe('sort', () => {
   it('sorts a simple array of 2, 3, 1', () => {
     const myArray = [2, 3, 1]
     expect(sort(myArray)).toEqual([1, 2, 3])
+  })
+
+  it('sorting is idempotent', () => {
+    const myArray = [2, 3, 1]
+    expect(sort(sort(myArray))).toEqual(sort(myArray))
+  })
+
+  it('sorting is idempotent - property based', () => {
+    fc.assert(
+      fc.property(
+        fc.array(fc.integer()),
+        myArray => sort(sort(myArray)) === sort(myArray)
+      )
+    )
   })
 })
 
